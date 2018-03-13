1a917690e084da16f88cbfa60454cdb18b8f3e71
fmd123 <fmdavis6@gmail.com>
2018-03-13 12:51:13 -0600

    functionality: App renders al the way to book. Structurally complete. Fixed: put key={book.title} into correct spot in Bookshelf (Should not be on Book.js) Ready to add interactivity :totoro:

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react'
-// import Book from './Book'
+import Book from './Book'
+
+
+
 class Bookshelf extends Component {
 
   render(){
 
     const {books} = this.props
 
-    console.log('inside render method', books)
+    console.log('inside render method-Bookshelf', {books})
 
 
 
@@ -18,27 +21,12 @@ class Bookshelf extends Component {
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.shelfName}</h2>
         <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map((book)=>(
-              <li key={book.title} className="current-list-item">
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors}</div>
-                </div>
 
-              </li>
+          <ol className="books-grid">
+            {console.log("inside return of render-Bookshelf", {books})}
+            {books.map((book)=>(
+              <Book book={book} key={book.title} />
+
             ))}
           </ol>
         </div>
