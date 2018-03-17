46cc5d06caa51a6942f86b611511bb2837f73f98
fmd123 <fmdavis6@gmail.com>
2018-03-17 16:53:25 -0600

    don't use BookshelfChanger component

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -13,19 +13,7 @@ class BookShelfChanger extends Component {
   render(){
     const {book, handleChange, shelf} = this.props
     return(
-      <form onSubmit={this.handleSubmit} className="book-shelf-changer">
-        {console.log({shelf})}
 
-        <select onChange={handleChange}>
-          <option value="none" disabled>Move to...</option>
-          <option value={"currentlyReading"}>Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-
-
-      </form>
     )
   }
 }
