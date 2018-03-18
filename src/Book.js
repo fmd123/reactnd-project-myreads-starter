4688c39d3c1b2159d33e1aeb97e7f6e8e9603f5b
fmd123 <fmdavis6@gmail.com>
2018-03-17 19:36:23 -0600

    still can't make  shelf change happen

--- src/Book.js
+++ src/Book.js
@@ -1,39 +1,42 @@
 import React, {Component} from 'react'
 // import BookShelfChanger from './BookShelfChanger'
-  class Book extends Component {
-
-    render(){
-      const{book, handleChange}= this.props
-      console.log(book)
-      const {shelf} = book.shelf
-       return (
-
-       <div className="book">
-         <div className="book-top">
-           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-           <div><form onSubmit={this.handleSubmit} className="book-shelf-changer">
+class Book extends Component {
 
 
-             <select onChange={(event) => this.props.handleChange(this.props.book, event.target.value)} defaultValue={ this.props.book.shelf }>
-               <option value="none" disabled>Move to...</option>
-               <option value={"currentlyReading"}>Currently Reading</option>
-               <option value="wantToRead">Want to Read</option>
-               <option value="read">Read</option>
-               <option value="none">None</option>
-             </select>
+  render() {
+    const {book, handleChange} = this.props
+    console.log(book)
+    const {shelf} = book.shelf
+    return (
 
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+          }}></div>
+          <div>
+            <form onSubmit={this.handleSubmit} className="book-shelf-changer">
 
-           </form></div>
-         </div>
-         <div className="book-title">{book.title}</div>
-         <div className="book-authors">{book.authors}</div>
+              <select value={this.props.shelf} onChange={(event)=>(handleChange(event.target.value))} >
+                <option value="none" disabled>Move to...</option>
+                <option value={"currentlyReading"}>Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
 
-       </div>
-       )
-    }
+            </form>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
 
+      </div>
+    )
   }
 
-
+}
 
 export default Book
