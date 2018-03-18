ae1a312352bd09014534df1ffe5b1551c0a99483
fmd123 <fmdavis6@gmail.com>
2018-03-17 21:01:37 -0600

    get I am message

--- src/App.js
+++ src/App.js
@@ -6,23 +6,17 @@ import Search from './Search'
 import ChangePage from './ChangePage'
 
 class BooksApp extends React.Component {
+  constructor(props) {
+      super(props);
 
-  state = {
-    books: [],
-
-      /**
-       * TODO: Instead of using this state variable to keep track of which page
-       * we're on, use the URL in the browser's address bar. This will ensure that
-       * users can use the browser's back and forward buttons to navigate between
-       * pages, as well as provide a good URL they can bookmark and share.
-       */
-      showSearchPage: false,
-      
-
+      this.state = {
+        books: [],
+        
+        showSearchPage: false,
+      };
+      this.handleChange = this.handleChange.bind(this);
 
     }
-
-
 
     handleChange = event => {
       {console.log("I am handleChange function")}
--- src/Book.js
+++ src/Book.js
@@ -18,7 +18,7 @@ class Book extends Component {
           }}></div>
           <div>
             <div onSubmit={this.handleSubmit} className="book-shelf-changer">
-              <select value={shelf} onChange={(event)=>(handleChange(event.target.value))} >
+              <select value={shelf} onChange={(event)=>(handleChange(event))} >
                 <option value="none" disabled>Move to...</option>
                 <option value={"currentlyReading"}>Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
